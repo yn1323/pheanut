549f7fdcfb75e8c45754bd27c5018702733ec402
yn1323 <yn1323@gmail.com>
2020-02-11 17:10:34 +0900

    Lint

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs-extra')
 const path = require('path')
 const packageDir = path.resolve(__dirname, '../../../')
-const pheanutDir = path.resolve(__dirname, '../');
+const pheanutDir = path.resolve(__dirname, '../')
 const exec = require('child_process').exec
 
 // ファイルのコピー
@@ -23,35 +23,36 @@ FILES.forEach(f => {
 
 // Scriptの追加
 fs.readFile(`${pheanutDir}/package.json`, (e, data) => {
-  if (e) throw e;
-  let scripts = JSON.parse(data).scripts;
+  if (e) throw e
+  let scripts = JSON.parse(data).scripts
   fs.readFile(`${packageDir}/package.json`, (err, data) => {
-    if (err) throw err;
-    const package = JSON.parse(data);
+    if (err) throw err
+    const package = JSON.parse(data)
     delete package.scripts.test
     Object.keys(scripts).forEach(k => {
-      package.scripts[k] = scripts[k];
+      package.scripts[k] = scripts[k]
     })
-    fs.writeFile(`${packageDir}/package.json`, JSON.stringify(package));
-  });
-});
+    fs.writeFile(`${packageDir}/package.json`, JSON.stringify(package))
+  })
+})
 
 // パッケージのインストール
 const PACKAGES = [
-  { type: '--save', lib: 'git://github.com/phinajs/phina.js.git#develop' },
-  { type: '--save-dev', lib: 'eslint' },
-  { type: '--save-dev', lib: 'fs-extra' },
-  { type: '--save-dev', lib: 'prettier' },
-  { type: '--save-dev', lib: 'webpack' },
-  { type: '--save-dev', lib: 'webpack-cli' },
-  { type: '--save-dev', lib: 'webpack-dev-server' }
-];
+  {type: '--save', lib: 'git://github.com/phinajs/phina.js.git#develop'},
+  {type: '--save-dev', lib: 'eslint'},
+  {type: '--save-dev', lib: 'fs-extra'},
+  {type: '--save-dev', lib: 'prettier'},
+  {type: '--save-dev', lib: 'webpack'},
+  {type: '--save-dev', lib: 'webpack-cli'},
+  {type: '--save-dev', lib: 'webpack-dev-server'}
+]
 
 PACKAGES.forEach(p => {
   console.log(`installing ${p.lib}...`)
   exec(`npm install ${p.type} ${p.lib}`, (e, stdout, stderr) => {
-    if (e) { console.log(e); }
-    console.log(stdout);
-  });
-});
-
+    if (e) {
+      console.log(e)
+    }
+    console.log(stdout)
+  })
+})
